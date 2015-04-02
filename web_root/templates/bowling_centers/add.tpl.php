4832a21f59c60d501e737f1d87ffe48f6a4aaba2
allenlin923 <allenlin923@gmail.com>
2015-04-02 02:25:51 -0500

    Added in base website stuff and added in bowling center CRUD files

--- web_root/templates/bowling_centers/add.tpl.php
+++ web_root/templates/bowling_centers/add.tpl.php
@@ -6,17 +6,11 @@ require_once $_TEMPLATES['location'] . 'header.tpl.php';
 
 <form action="" method="post">
     <label for="name">Bowling Center Name:</label>
-    <input type="text" maxlength="50" name="bowling_center_name" value="<?=$_POST['bowling_center_name']?>" />
-    <? if (isset($_TEMPLATES['vars']['form_errors']['bowling_center_name'])): ?>
-        <span class="error"><?= $_TEMPLATES['vars']['form_errors']['bowling_center_name'] ?></span>
+    <input type="text" maxlength="50" name="center_name" value="<?=$_POST['center_name']?>" />
+    <? if (isset($_TEMPLATES['vars']['form_errors']['center_name'])): ?>
+        <span class="error"><?= $_TEMPLATES['vars']['form_errors']['center_name'] ?></span>
     <? endif; ?>
 
-    <label for="file">PDF File:</label>
-    <input type="file" name="file" />
-    <? if (isset($_TEMPLATES['vars']['form_errors']['file'])): ?>
-        <span class="error"><?= $_TEMPLATES['vars']['form_errors']['file'] ?></span>
-    <? endif; ?>
-    
     <label for="location">Location:</label>
     <input type="text" maxlength="100" name="location" value="<?=$_POST['location']?>" />
     <? if (isset($_TEMPLATES['vars']['form_errors']['location'])): ?>
