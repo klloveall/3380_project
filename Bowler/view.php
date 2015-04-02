85b40136b8888a6b173c5ad39befbeeaf8f180c8
Tuan Le <tuanvanle21@gmail.com>
2015-04-02 11:22:01 -0500

    Update and rename Bowlers to view.php

---
+++ Bowler/view.php
@@ -0,0 +1,28 @@
+<?php
+/*$query = "
+		SELECT 
+				'game'.'game_number',
+				'game'.'score',
+				'users'.'first_name',
+				'users'
+				
+				
+				
+				
+				"
+*/
+
+$firstname = '';
+$lastname = '';
+$midname = '';
+$cellphone = '';
+$email_address = '';
+$game_number = '';
+
+echo "Name of player: $firstname $lastname $midname" <br>;
+echo "Phone number: $cellphone" <br>;
+echo "Email address: $email_address" <br>;
+
+
+}
+?>
