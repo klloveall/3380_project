62880c1c72b6ca8c707206dc0e13d33354b8e7dd
allenlin923 <allenlin923@gmail.com>
2015-04-02 02:27:41 -0500

    Added in base website stuff and added in bowling center CRUD files

--- web_root/templates/bowling_centers/listing.tpl.php
+++ web_root/templates/bowling_centers/listing.tpl.php
@@ -8,9 +8,9 @@ require_once $_TEMPLATES['location'] . 'header.tpl.php';
     <h2><a href='?id=<?=$centers['id']?>'><?=$centers['name']?></a></h2>
     <p>Notes: <?=$centers['notes']?></p>
     <p>
-        <a href="view_bowling_centers.php?id=<?=$centers['id']?>">View Bowling Centers</a><br />
-        <a href="edit_bowling_centers.php?id=<?=$centers['id']?>">Edit Bowling Centers</a><br />
-        <a class="delete" href="edit_bowling_centers.php?id=<?=$centers['id']?>&delete=true">Delete Bowling Centers</a><br />
+        <a href="view_bowling_center.php?id=<?=$centers['id']?>">View Bowling Centers</a><br />
+        <a href="edit_bowling_center.php?id=<?=$centers['id']?>">Edit Bowling Centers</a><br />
+        <a class="delete" href="edit_bowling_center.php?id=<?=$centers['id']?>&delete=true">Delete Bowling Centers</a><br />
     </p>
 
 <?php endforeach; ?>
