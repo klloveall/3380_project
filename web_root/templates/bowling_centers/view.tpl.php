4bcded4934b37d5fcc67775d354e8d962daf25c5
allenlin923 <allenlin923@gmail.com>
2015-04-02 02:28:10 -0500

    Added in base website stuff and added in bowling center CRUD files

--- web_root/templates/bowling_centers/view.tpl.php
+++ web_root/templates/bowling_centers/view.tpl.php
@@ -2,8 +2,7 @@
 require_once $_TEMPLATES['location'] . 'header.tpl.php';
 ?>
 <h1>View Bowling Center</h1>
-<p><b>Bowling Center Name:</b> <?= $_POST['bowling_center_name'] ?></p>
-<p><b>PDF File:</b> Currently disabled</p>
+<p><b>Bowling Center Name:</b> <?= $_POST['center_name'] ?></p>
 <p><b>Location: </b> <?= $_POST['location']?></p>
 <p><b>Notes:</b> <?= $_POST['notes'] ?></p>
 
