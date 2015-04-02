f37654e9a90170966848325de3de0ef202742310
Tuan Le <tuanvanle21@gmail.com>
2015-04-02 11:22:28 -0500

    Create main.php

---
+++ Bowler/main.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<form action = "edit.php" method = "get">
+
+<b>Enter your first name: </b> <input type = "text" name = "firstname"> <br><br>
+<b>Enter your last name: </b> <input type = "text" name = "lastname"> <br><br>
+<b>Enter you middle name: </b> <input type = "text" name = "midname"> <br><br>
+<b>Enter your phone number: </b> <input type = "number" name = "phonenumber"> <br><br>
+<b>Enter your email address: </b> <input type = "text" name = "email"> <br><br>
+
+<input type = "submit">
+
+</form>
+
+</body>
+</html>
