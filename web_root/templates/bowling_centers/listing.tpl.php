164d7eef9cdb30292adb4a417bc075ef48f73a9e
allenlin923 <allenlin923@gmail.com>
2015-04-02 02:11:29 -0500

    Added in base website stuff and added in bowling center CRUD files

---
+++ web_root/templates/bowling_centers/listing.tpl.php
@@ -0,0 +1,20 @@
+<?php
+require_once $_TEMPLATES['location'] . 'header.tpl.php';
+?>
+<h1>Bowling Centers</h1>
+
+<?php foreach ($_TEMPLATES['vars']['centers'] as $centers): ?>
+    <hr />
+    <h2><a href='?id=<?=$centers['id']?>'><?=$centers['name']?></a></h2>
+    <p>Notes: <?=$centers['notes']?></p>
+    <p>
+        <a href="view_bowling_centers.php?id=<?=$centers['id']?>">View Bowling Centers</a><br />
+        <a href="edit_bowling_centers.php?id=<?=$centers['id']?>">Edit Bowling Centers</a><br />
+        <a class="delete" href="edit_bowling_centers.php?id=<?=$centers['id']?>&delete=true">Delete Bowling Centers</a><br />
+    </p>
+
+<?php endforeach; ?>
+    
+<?php
+require_once $_TEMPLATES['location'] . 'footer.tpl.php';
+?>
