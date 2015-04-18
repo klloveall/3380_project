0f5dee14baf284972c36760336a51c3a900fbfe9
zaidkh <zaidkhayat@Zaids-MacBook-Pro.local>
2015-04-18 15:40:18 -0500

    temp balls

---
+++ web_root/templates/bowling_balls/add.tpl.php
@@ -0,0 +1,31 @@
+<?php
+require_once $_TEMPLATES['location'] . 'header.tpl.php';
+?>
+
+<h1>Add New Bowling Ball</h1>
+
+<form action="" method="post">
+    <label for="name">Bowling Ball Name:</label>
+    <input type="text" maxlength="50" name="ball_name" value="<?=$_POST['ball']?>" />
+    <? if (isset($_TEMPLATES['vars']['form_errors']['ball_name'])): ?>
+        <span class="error"><?= $_TEMPLATES['vars']['form_errors']['ball_name'] ?></span>
+    <? endif; ?>
+
+    <label for="file">PDF File:</label>
+    <input type="file" name="file" />
+    <? if (isset($_TEMPLATES['vars']['form_errors']['file'])): ?>
+        <span class="error"><?= $_TEMPLATES['vars']['form_errors']['file'] ?></span>
+    <? endif; ?>
+
+    <label for="notes">Notes:</label>
+    <textarea name="notes"><?=$_POST['notes']?></textarea>
+    <? if (isset($_TEMPLATES['vars']['form_errors']['notes'])): ?>
+        <span class="error"><?= $_TEMPLATES['vars']['form_errors']['notes'] ?></span>
+    <? endif; ?>
+
+    <input type="submit" name="submit" value="Add Bowling Ball" />
+</form>
+
+<?php
+require_once $_TEMPLATES['location'] . 'footer.tpl.php';
+?>
