0159b2453cf80f3aa4d13bb104e6645eadec58d0
avikgemini <avikgemini@gmail.com>
2015-04-27 21:36:22 -0500

    update view template

--- web_root/templates/bowling_teams/view.tpl.php
+++ web_root/templates/bowling_teams/view.tpl.php
@@ -6,9 +6,13 @@ require_once $_TEMPLATES['location'] . 'header.tpl.php';
 
 <p><b>Team Id:</b> <?= $_TEMPLATES['vars']['teams_id'] ?></p>
 <p><b>Team Name:</b> <?= $_TEMPLATES['vars']['name'] ?></p> 
-<p><b>Team Owner:</b> <?= $_TEMPLATES['vars']['owners'] ?></p>
+<p><b>Team Owner:</b> <?= $_TEMPLATES['vars']['owner_name'] ?></p>
 <p><b>Team Notes:</b> <?= $_TEMPLATES['vars']['notes'] ?></p>
 <p><b>Team Members:</b>
+    <?php foreach($_TEMPLATES['vars']['members'] as $member): ?>
+    <?= $member?>, 
+    <?php endforeach; ?>
+</p>
 
 <!--<? //while($data_two= $results_two->fetch_array()) {?>
     <p><b>Team Member:</b><?= $data_two['preferred_name'] ?> <?= $data_two['last_name'] ?></p> 
